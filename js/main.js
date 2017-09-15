8653fb95e2c85494629dcf6844d2aa1b91977985
seb-faull <seb_faull@hotmail.co.uk>
2017-09-15 16:50:43 +0100

    Added background image and implemented jQuery to animate the targets

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(function() {
 
 	console.log('Dom has loaded');
 
+	animateDiv()
+
 	$('.green-target').on('click', function (event) {
 		console.log('Greens been clicked');
 		var $greenTarget = $('.green-target');
@@ -24,3 +26,33 @@ $(function() {
 
 
 });
+
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $('#arena').height() - 50;
+    var w = $('#arena').width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv () {
+	var newq = makeNewPosition();
+	$('.green-target').animate({ top: newq[0], left: newq[1] }, function(){
+		animateDiv();
+	})
+
+	$('.blue-target').animate({ top: newq[0], left: newq[1] }, function(){
+		animateDiv();
+	})
+
+	$('.red-target').animate({ top: newq[0], left: newq[1] }, function(){
+		animateDiv();
+	})
+}
+
