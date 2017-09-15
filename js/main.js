787296c07eaba629c95b86f7d977d92eefa52de8
seb-faull <seb_faull@hotmail.co.uk>
2017-09-15 15:31:46 +0100

    Added a Green, Blue and Red Target to the arena, having implemented their own css classes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(function() {
-	
+
 	console.log('Dom has loaded');
 
+	$('#target').on('click', function (event) {
+		console.log('I have been clicked!');
+		var $target = $('#target');
+
+	});
 
 
 
