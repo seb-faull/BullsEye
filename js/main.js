f4c2a5a8979067f18c0c967584b8d562409427c6
seb-faull <seb_faull@hotmail.co.uk>
2017-09-15 15:36:08 +0100

    Implemented on click event listeners to each target

--- js/main.js
+++ js/main.js
@@ -2,9 +2,21 @@ $(function() {
 
 	console.log('Dom has loaded');
 
-	$('#target').on('click', function (event) {
-		console.log('I have been clicked!');
-		var $target = $('#target');
+	$('.green-target').on('click', function (event) {
+		console.log('Greens been clicked');
+		var $greenTarget = $('.green-target');
+
+	});
+
+	$('.blue-target').on('click', function (event) {
+		console.log('Blues been clicked!');
+		var $blueTarget = $('.blue-target');
+
+	});
+
+	$('.red-target').on('click', function (event) {
+		console.log('Reds been clicked!');
+		var $redTarget = $('.red-target');
 
 	});
 
